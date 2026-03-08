2199d0b649c522250d919edd8d6758cd3c1128e0
VMHETAR <mhetarvarad@gmail.com>
2026-03-08 13:54:19 +0530

    Updated the code

--- encapsulation.c
+++ encapsulation.c
@@ -1,5 +1,26 @@
 /*
-Encapsulation is an OOP principle which allows the bundling of data and methods in a single unit like say a class.
+Group data and functions together
 */
 #include <stdio.h>
 #include<conio.h>
+
+typedef struct{
+    int value;
+}Counter;
+
+void increment(Counter *c){
+    c->value++;
+}
+
+void print(Counter *c){
+    printf("Counter value: %d\n", c->value);
+}
+
+int main(){
+    Counter c = {0};  //Initializing the counter to 0 
+    increment(&c);
+    increment(&c);
+    
+    print(&c);
+    return 0;
+}
